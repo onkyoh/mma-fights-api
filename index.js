a8a73d14c6238178526074a61bd8f688e2705f5f
onkyoh <adnanradwan_8@hotmail.com>
2023-03-31 13:49:49 -0400

    fixed cron

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ const updatedDb = async () => {
 
 //cron job to scrape everyday
 
-const runScrape = new CronJob('52 13 * * *', () => {
+const runScrape = new CronJob('55 13 * * *', () => {
   updatedDb()
 })
 
