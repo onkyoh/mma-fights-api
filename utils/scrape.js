52df2a3d84380d87e3d2d2c27a722059fb3dd8f9
onkyoh <adnanradwan_8@hotmail.com>
2024-04-03 23:53:37 -0400

    they changed to tailwind zzz

--- utils/scrape.js
+++ utils/scrape.js
@@ -33,58 +33,75 @@ const scrape = async () => {
     const eventResponse = await axios.get(event.link);
     const $event = cheerio.load(eventResponse.data);
 
-    const fights = $event("li.fightCard:not(.picks)")
+    const fights = $event("li.border-b.border-dotted.border-tap_6")
       .map((_, el) => {
         const main = $event(el)
-          .find(".billing")
+          .find(
+            "a.hover\\:border-solid.hover\\:border-b.hover\\:border-neutral-950.hover\\:text-neutral-950"
+          )
           .text()
           .toLowerCase()
-          .includes("main")
-          ? true
-          : false;
+          .includes("main");
+
+        // Extract the weight class
         const weight = $event(el)
-          .find(".fightCardWeight .weight")
+          .find(
+            "span.bg-tap_darkgold.px-1\\.5.md\\:px-1.leading-\\[23px\\].text-sm.md\\:text-\\[13px\\].text-neutral-50.rounded"
+          )
           .text()
           .trim();
 
+        // Find the container that encapsulates each fighter's info
+        const fighterContainers = $event(el).find(
+          ".div.flex.flex-row.gap-0\\.5.md\\:gap-0.w-full"
+        );
+
+        // Extracting Fighter A's Information
+        const fighterAContainer = fighterContainers.eq(0); // First fighter container
         const fighterA = {
-          name: $event(el).find(".fightCardFighterName.left a").text().trim(),
-          record: $event(el)
-            .find(".fightCardFighterBout.left .fightCardRecord")
+          name: fighterAContainer.find(".link-primary-red").text().trim(),
+          record: fighterAContainer
+            .find(".text-\\[15px\\].md\\:text-xs.order-2")
             .text()
             .trim(),
           country:
             baseUrl +
-            $event(el)
-              .find(".fightCardFighterBout.left .fightCardFlag")
+            fighterAContainer
+              .find(
+                ".opacity-70.h-\\[14px\\].md\\:h-\\[11px\\].w-\\[22px\\].md\\:w-\\[17px\\]"
+              )
               .attr("src"),
-          picture: $event(el)
-            .find(".fightCardFighterImage img")
-            .first()
+          picture: fighterAContainer
+            .find(
+              ".w-\\[77px\\].h-\\[77px\\].md\\:w-\\[104px\\].md\\:h-\\[104px\\].rounded"
+            )
             .attr("src"),
           link:
-            baseUrl +
-            $event(el).find(".fightCardFighterBout.left a").attr("href"),
+            baseUrl + fighterAContainer.find(".link-primary-red").attr("href"),
         };
 
+        // Extracting Fighter B's Information
+        const fighterBContainer = fighterContainers.eq(1); // Second fighter container, assuming it's structured similarly
         const fighterB = {
-          name: $event(el).find(".fightCardFighterName.right a").text().trim(),
-          record: $event(el)
-            .find(".fightCardFighterBout.right .fightCardRecord")
+          name: fighterBContainer.find(".link-primary-red").text().trim(),
+          record: fighterBContainer
+            .find(".text-\\[15px\\].md\\:text-xs.order-1")
             .text()
             .trim(),
           country:
             baseUrl +
-            $event(el)
-              .find(".fightCardFighterBout.right .fightCardFlag")
+            fighterBContainer
+              .find(
+                ".opacity-70.h-\\[14px\\].md\\:h-\\[11px\\].w-\\[22px\\].md\\:w-\\[17px\\]"
+              )
               .attr("src"),
-          picture: $event(el)
-            .find(".fightCardFighterImage img")
-            .last()
+          picture: fighterBContainer
+            .find(
+              ".w-\\[77px\\].h-\\[77px\\].md\\:w-\\[104px\\].md\\:h-\\[104px\\].rounded"
+            )
             .attr("src"),
           link:
-            baseUrl +
-            $event(el).find(".fightCardFighterBout.right a").attr("href"),
+            baseUrl + fighterBContainer.find(".link-primary-red").attr("href"),
         };
 
         return { main, weight, fighterA, fighterB };
